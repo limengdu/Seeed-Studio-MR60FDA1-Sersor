6c4ca2240823ca7b93c8c5750e9835afc2898e54
limengdu <37475446+limengdu@users.noreply.github.com>
2023-02-07 16:11:17 +0800

    first update

--- 60ghzfalldetection.cpp
+++ 60ghzfalldetection.cpp
@@ -1,185 +1,180 @@
 #include "Arduino.h"
 #include "60ghzfalldetection.h"
 
-#ifdef __AVR__
-    #include <SoftwareSerial.h>
-    SoftwareSerial SSerial(2, 3); // RX, TX
-    #define Serial1 SSerial
-#endif
-
-void FallDetection_60GHz::SerialInit(){
-  Serial1.begin(115200);
+void FallDetection_60GHz::FallDetection_60GHz(Stream *s)
+    : stream(s){
+  this->newData = false;
 }
 
 // Receive data and process
 void FallDetection_60GHz::recvRadarBytes(){
-  static boolean recvInProgress = false;
-  static byte ndx = 0;
-  byte startMarker = MESSAGE_HEAD;            //Header frame
-  byte endMarker = MESSAGE_TAIL;
-  byte rb; // Each frame received
-  while (Serial1.available() > 0 && newData == false)
-  {
-    rb = Serial1.read();
-    if (recvInProgress == true)
-    {                     // Received header frame
-      if (rb != endMarker){ // Length in range
-        Msg[ndx] = rb;
-        ndx++;
+  while (stream->available()) {
+    if(stream->read() == MESSAGE_HEAD1){           //Receive header frame 1
+      if(stream->read() == MESSAGE_HEAD2){         //Receive header frame 2
+        dataLen = stream->readBytesUntil(MESSAGE_END2, Msg, 20);
+        if (dataLen > 0 && dataLen < 20){
+          Msg[dataLen] = MESSAGE_END2;
+          this->newData = true;
+        }
       }
-      else{
-        recvInProgress = false;
-        dataLen = ndx;
-        ndx = 0;
-        newData = true;
-      }
-    }
-    else if (rb == startMarker){ // Waiting for the first frame to arrive
-        recvInProgress = true;
     }
   }
 }
 
 //Radar transmits data frames for display via serial port
-void FallDetection_60GHz::ShowData(byte inf[]){
-  for (byte n = 0; n < dataLen+3; n++) {
-    Serial.print(inf[n], HEX);
+void FallDetection_60GHz::showData(){
+  if(this->newData){
+    Serial.print(MESSAGE_HEAD1, HEX);
     Serial.print(' ');
-  }
+    Serial.print(MESSAGE_HEAD2, HEX);
+    Serial.print(' ');
+    char charVal[4];
+    for (byte n = 0; n < dataLen+1; n++) {
+      sprintf(charVal, "%02X", Msg[n]);
+      Serial.print(charVal);
+      Serial.print(' ');
+    }
     Serial.println();
+    this->newData = false;
+    Msg[dataLen] = {0};
+  }
 }
 
-
 // Judgment of occupied and unoccupied, approach and distance
-void FallDetection_60GHz::Situation_judgment(byte inf[]){
-  switch(inf[2]){
-    case HUMAN_PSE_RADAR:
-      switch(inf[3]){
-        case PRESENCE_INF:
-          switch(inf[6]){
-            case NOONE_HERE:
-              ShowData(inf);
-              Serial.println("Radar detects no one.");
-              Serial.println("----------------------------");
-              break;
-            case SOMEONE_HERE:
-              ShowData(inf);
-              Serial.println("Radar detects somebody.");
-              Serial.println("----------------------------");
-              break;
-          }
-          break;
-        case MOVE_INF:
-          switch(inf[6]){
-            case NONE:
-              ShowData(inf);
-              Serial.println("Radar detects None.");
-              Serial.println("----------------------------");
-              break;
-            case STATIONARY:
-              ShowData(inf);
-              Serial.println("Radar detects somebody stationary.");
-              Serial.println("----------------------------");
-              break;
-            case MOVEMENT:
-              ShowData(inf);
-              Serial.println("Radar detects somebody in motion.");
-              Serial.println("----------------------------");
-              break;
-          }
-          break;
-        case BODY_SIG:
-          ShowData(inf);
-          Serial.print("The radar identifies the current motion feature value is: ");
-          Serial.println(inf[6]);
-          Serial.println("----------------------------");
-          break;
-        case DISTANCE:
-          ShowData(inf);
-          Serial.print("The distance of the radar from the monitored person is: ");
-          Serial.print(inf[6]);
-          Serial.print(" ");
-          Serial.print(inf[7]);
-          Serial.println(" cm");
-          Serial.println("----------------------------");
-          break;
-        case MOVESPEED:
-          ShowData(inf);
-          Serial.print("The speed of movement of the monitored person is: ");
-          Serial.print(inf[6]);
-          Serial.print(" ");
-          Serial.print(inf[7]);
-          Serial.println(" cm/s");
-          Serial.println("----------------------------");
-          break;
-      }
-      break;
+void FallDetection_60GHz::HumanExis_Func(){
+  recvRadarBytes();
+  reset_val();
+  if(this->newData){
+    switch(Msg[0]){
+      case HUMAN_PSE_RADAR:
+        switch(Msg[1]){
+          case PRESENCE_INF:
+            switch(Msg[4]){
+              case NOONE_HERE:
+                showData();
+                sensor_report = NOONE;
+                break;
+              case SOMEONE_HERE:
+                showData();
+                sensor_report = SOMEONE;
+                break;
+            }
+            break;
+          case MOVE_INF:
+            switch(Msg[4]){
+              case PSE_NONE:
+                showData();
+                sensor_report = NONEPSE;
+                break;
+              case STATIONARY:
+                showData();
+                sensor_report = STATION;
+                break;
+              case MOVEMENT:
+                showData();
+                sensor_report = MOVE;
+                break;
+            }
+            break;
+          case BODY_SIG:
+            showData();
+            sensor_report = BODYVAL;
+            bodysign_val = Msg[4];
+            break;
+          // case MOVE_DIS:  //This data is only used as an interface and is not normally required by users
+          //   showData();
+          //   sensor_report = MOVEDIS;
+          //   total_detect = (Msg[4] << 8 | Msg[5]);
+          //   height_0005 = Msg[6];
+          //   height_0510 = Msg[7];
+          //   height_1015 = Msg[8];
+          //   height_1520 = Msg[9];
+          //   break;
+        }
+        break;
+    }
   }
 }
 
 //Respiratory sleep data frame decoding
-void FallDetection_60GHz::Fall_Detection(byte inf[]){
-  switch(inf[2]){
-    case FALL_DETECTION:
-      switch(inf[3]){
-        case FALL_STATE:
-          switch(inf[6]){
-            case NO_FALL:
-              ShowData(inf);
-              Serial.println("Radar detects that the current no fall.");
-              Serial.println("----------------------------");
-              break;
-            case FALLING:
-              ShowData(inf);
-              Serial.println("Radar detects current someone falling.");
-              Serial.println("----------------------------");
-              break;
-          }
-          break;
-        case FALL_POTENTIAL:
-          ShowData(inf);
-          Serial.print("The confidence level for the current radar detection of a fall is: ");
-          Serial.println(inf[6]);
-          Serial.println("----------------------------");
-          break;
-        case FALL_LOCATION:
-          ShowData(inf);
-          Serial.print("The fall position is: ");
-          Serial.print("x: ");
-          Serial.print(inf[6]);
-          Serial.print(" ");
-          Serial.print(inf[7]);
-          Serial.print(" ");
-          Serial.print("y: ");
-          Serial.print(inf[8]);
-          Serial.print(" ");
-          Serial.println(inf[9]);
-          Serial.println("----------------------------");
-          break;
-        case POINTCLOUD_DATA:
-          ShowData(inf);
-          Serial.print("The point cloud data are: ");
-          Serial.print("x: ");
-          Serial.print(inf[6]);
-          Serial.print(" ");
-          Serial.print(inf[7]);
-          Serial.print(" ");
-          Serial.print("y: ");
-          Serial.print(inf[8]);
-          Serial.print(" ");
-          Serial.print(inf[9]);
-          Serial.print(" ");
-          Serial.print("z: ");
-          Serial.print(inf[10]);
-          Serial.print(" ");
-          Serial.println(inf[11]);
-          Serial.println("----------------------------");
-          break;
-      }
-      break;
+void FallDetection_60GHz::Fall_Detection(){
+  recvRadarBytes();
+  reset_val();
+  if(this->newData){
+    switch(Msg[0]){
+      case FALL_DETECTION:
+        switch(Msg[1]){
+          case FALL_STATE:
+            switch(Msg[4]){
+              case NO_FALL:
+                showData();
+                sensor_report = NOFALL;
+                break;
+              case FALLING:
+                showData();
+                sensor_report = FALL;
+                break;
+            }
+            break;
+          case RESIDENT_STATE:
+            switch(Msg[4]){
+              case NO_RESIDENT:
+                showData();
+                sensor_report = NORESIDENT;
+                break;
+              case RESIDENT:
+                showData();
+                sensor_report = RESIDENCY;
+                break;
+            }
+            break;
+        }
+        break;
+    }
   }
 }
 
+//Send data frame
+void FallDetection_60GHz::send_func(const unsigned char* buff, int len, bool cyclic /*=false*/){
+  if(cyclic || count < checkdata_len){
+    if(cyclic || count < 1){
+      stream->write(buff, len);
+      stream->flush();
+    }
+    do{
+      recvRadarBytes();
+      delay(20);
+    }while(!(this->newData));
+    if(cyclic || count < 1){
+      Serial.print("  Sent  ---> ");
+      data_printf(buff, len);
+    }
+    if(count%2 == 1){
+      Serial.print("Receive <--- ");
+      showData();
+    }
+    this->newData = false;
+  }
+  count++;
+}
 
+//Reset radar
+void FallDetection_60GHz::reset_func(){
+  stream->write(reset_frame, reset_frame_len);
+  stream->flush();
+  Serial.println("Radar reset!");
+}
+
+//reset the radar values
+void FallDetection_60GHz::reset_val(){
+  sensor_report = 0x00;
+
+  bodysign_val = 0x00;
+  total_detect = 0x00;
+  height_0005 = 0x00;
+  height_0510 = 0x00;
+  height_1015 = 0x00;
+  height_1520 = 0x00;
+}
 
 
--- 60ghzfalldetection.h
+++ 60ghzfalldetection.h
@@ -1,8 +1,11 @@
 #ifndef _RADAR_H__
 #define _RADAR_H__
 
-#define MESSAGE_HEAD 0x53       //Data frame header
-#define MESSAGE_TAIL 0x54       //Data frame tail
+#define MESSAGE_HEAD1 0x53      //Data frame header1
+#define MESSAGE_HEAD2 0x59      //Data frame header2
+
+#define MESSAGE_END1  0x54      //End1 of data frame
+#define MESSAGE_END2  0x43      //End2 of data frame
 
 #define HUMAN_PSE_RADAR 0x80    //Human presence data
 
@@ -11,15 +14,13 @@
 #define NOONE_HERE 0x00         //Noone here
 
 #define MOVE_INF 0x02           //Campaign Information
-#define NONE 0x00               //None
+#define PSE_NONE 0x00           //None
 #define STATIONARY 0x01         //A person is stationary
 #define MOVEMENT 0x02           //A person in motion
 
 #define BODY_SIG 0x03           //Body movement information
 
-#define DISTANCE 0x04           //Distance from the person being detected
-
-#define MOVESPEED 0x06          //Speed of character movement
+#define MOVE_DIS 0x0E           //Determine the distance at which the movement occurs
 
 #define FALL_DETECTION 0x83     //Fall data markers
 
@@ -27,26 +28,46 @@
 #define NO_FALL 0x00            //No falls detected
 #define FALLING 0x01            //Fall detected
 
-#define FALL_POTENTIAL 0x02     //Confidence level for falls
+#define RESIDENT_STATE 0x05     //Resident status
+#define NO_RESIDENT 0x00        //No stationary residency
+#define RESIDENT 0x01           //There is a stationary residency
 
-#define FALL_LOCATION 0x03      //Location of the fall
+#define reset_frame_len 10      //Reset data frame length
+//Reset data frame
+const unsigned char reset_frame[10] = {0x53, 0x59, 0x01, 0x02, 0x00, 0x01, 0x0F, 0xBF, 0x54, 0x43};
 
-#define POINTCLOUD_DATA 0x04    //Point cloud data
+//Return status, Use in arduino
+#define NOONE 0x01
+#define SOMEONE 0x02
+#define NONEPSE 0x03
+#define STATION 0x04
+#define MOVE 0x05
+#define BODYVAL 0x06
+#define MOVEDIS 0x07
+
+#define NOFALL 0x08
+#define FALL 0x09
+#define NORESIDENT 0x10
+#define RESIDENCY 0x11
 
 
 class FallDetection_60GHz{
     private:
-        
+        Stream *stream;               // e.g. SoftwareSerial or Serial1
+        boolean newData;
+        byte dataLen;
+        unsigned char Msg[20] = {0};  //Storage of the data frames returned by the radar up to a maximum length of 20.
+        int count = 0;
+        int checkdata_len = 2;        //Without cyclic sending, number of frames sent
+        void reset_val();
     public:
-        const byte MsgLen = 12;
-        byte dataLen = 12;
-        byte Msg[12];
-        boolean newData = false;
-        void SerialInit();
+        unsigned int sensor_report = 0, bodysign_val = 0, total_detect = 0, height_0005 = 0, height_0510 = 0, height_1015 = 0, height_1520 = 0;
         void recvRadarBytes();
-        void Fall_Detection(byte inf[]);
-        void Situation_judgment(byte inf[]);
-        void ShowData(byte inf[]);
+        void showData();
+        void HumanExis_Func();
+        void Fall_Detection();
+        void send_func(const unsigned char* buff, int len, bool cyclic = false);
+        void reset_func();
 };
 
 #endif
